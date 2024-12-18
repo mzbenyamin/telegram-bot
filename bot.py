8d883b9f4783b4d80b5acb791ada42e4b3e86fd4
benyamin <134231188+mzbenyamin@users.noreply.github.com>
2024-12-18 22:12:53 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,11 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import os
+import telebot
 
-# توکن ربات تلگرام شما
-TELEGRAM_TOKEN = 'TELEGRAM_BOT_TOKEN'
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # گرفتن توکن از متغیر محیطی
+bot = telebot.TeleBot(TOKEN)
 
-# تابعی برای پاسخ به پیام‌های متنی
-def handle_message(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("سلام خوبی جیگر؟ حالت چطوره؟")
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "سلام! ربات شما فعال شد!")
 
-# تابع اصلی که ربات رو اجرا می‌کنه
-def main() -> None:
-    # ایجاد Updater و ربات با توکن
-    updater = Updater(TELEGRAM_TOKEN)
-
-    # دریافت dispatcher برای ثبت هندلرها
-    dispatcher = updater.dispatcher
-
-    # هندلر برای پیام‌های متنی
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
-
-    # شروع ربات
-    updater.start_polling()
-
-    # ربات همیشه در حال فعالیت باشه
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+bot.polling()

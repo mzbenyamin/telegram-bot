0ffe3104f7b1252b8daa8535731df3a013429733
benyamin <134231188+mzbenyamin@users.noreply.github.com>
2024-12-18 22:16:27 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,6 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "سلام! ربات شما فعال شد!")
+    bot.reply_to(message, "شد!")
 
 bot.polling()
